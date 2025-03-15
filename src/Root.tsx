d320fae1a54152664f5675e7b0e115f5178de494
Nauman Mustafa <nauman.mustafa.x@gmail.com>
2025-03-15 10:45:08 +0500

    Remove unnecessary comments from configuration and Root component files

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,22 +2,17 @@ import './index.css';
 import { Composition } from "remotion";
 import { HelloWorld, myCompSchema } from "./HelloWorld";
 
-// Each <Composition> is an entry in the sidebar!
-
 export const RemotionRoot: React.FC = () => {
   return (
     <>
       <Composition
-        // You can take the "id" to render a video:
-        // npx remotion render src/index.ts <id> out/video.mp4
+        // Must never change the schema ID
         id="MainVideo"
         component={HelloWorld}
         durationInFrames={150}
         fps={30}
         width={1920}
         height={1080}
-        // You can override these props for each render:
-        // https://www.remotion.dev/docs/parametrized-rendering
         schema={myCompSchema}
         defaultProps={{
           titleText: "Welcome to Remotion",
