2ab13193573c785fd966b062b3cc2f5f07cf413f
Nauman Mustafa <nauman.mustafa.x@gmail.com>
2025-03-15 10:20:47 +0500

    Update dependencies and modify project structure
    - Updated Remotion and related dependencies to version 4.0.278
    - Added 'build' to .gitignore
    - Changed composition ID from 'HelloWorld' to 'MainVideo' in Root.tsx
    - Removed unused Logo component from Root.tsx

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,6 +1,5 @@
 import { Composition } from "remotion";
 import { HelloWorld, myCompSchema } from "./HelloWorld";
-import { Logo, myCompSchema2 } from "./HelloWorld/Logo";
 
 // Each <Composition> is an entry in the sidebar!
 
@@ -10,7 +9,7 @@ export const RemotionRoot: React.FC = () => {
       <Composition
         // You can take the "id" to render a video:
         // npx remotion render src/index.ts <id> out/video.mp4
-        id="HelloWorld"
+        id="MainVideo"
         component={HelloWorld}
         durationInFrames={150}
         fps={30}
@@ -24,21 +23,6 @@ export const RemotionRoot: React.FC = () => {
           titleColor: "#000000",
           logoColor1: "#91EAE4",
           logoColor2: "#86A8E7",
-        }}
-      />
-
-      {/* Mount any React component to make it show up in the sidebar and work on it individually! */}
-      <Composition
-        id="OnlyLogo"
-        component={Logo}
-        durationInFrames={150}
-        fps={30}
-        width={1920}
-        height={1080}
-        schema={myCompSchema2}
-        defaultProps={{
-          logoColor1: "#91dAE2" as const,
-          logoColor2: "#86A8E7" as const,
         }}
       />
     </>
